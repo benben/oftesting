9b614729190988322a1e355a542421cd517bbfa4
Benjamin Knofe <ben@nerdlabor.de>
2012-10-29 01:53:19 +0100

    persist new jobs in db

--- check_for_work.rb
+++ check_for_work.rb
@@ -3,6 +3,7 @@ $:.unshift File.expand_path(File.dirname(__FILE__))
 require 'json'
 require 'github_api'
 require 'active_record'
+require 'lib/job'
 ActiveRecord::Base.establish_connection(YAML.load_file('config.yml')['db'])
 
 login = YAML.load_file('login.yml')
@@ -11,15 +12,23 @@ repo  = 'openFrameworks'
 
 github = Github.new basic_auth: "#{login['user']}:#{login['pass']}"
 
-#get latest sha's from open PR's
-
-pull_requests = []
-
-github.pull_requests.list(user, repo, state: 'open') do |pr|
-  temp = github.pull_requests.get(user, repo, pr.number)
-  pull_requests << temp if temp.mergeable
+#add new jobs
+github.pull_requests.list(user, repo, state: 'open') do |pull_request|
+  pr = github.pull_requests.get(user, repo, pull_request.number)
+  if pr.mergeable
+    str = "#{pr.number} - #{pr.title} - #{pr.head.repo.ssh_url} - #{pr.head.ref} - #{pr.head.sha}"
+    uid = Digest::SHA1.hexdigest(str)
+    unless Job.find_by_uid(uid)
+      Job.create!({
+        uid:       uid,
+        test_type: 'pr',
+        priority:  10,
+        number:    pr.number,
+        title:     pr.title,
+        ssh_url:   pr.head.repo.ssh_url,
+        branch:    pr.head.ref,
+        commit:    pr.head.sha,
+      })
+    end
+  end
 end
-
-
-require 'pry'
-binding.pry
--- lib/db/20121029011452_create_jobs.rb
+++ lib/db/20121029011452_create_jobs.rb
@@ -1,8 +1,14 @@
 class CreateJobs < ActiveRecord::Migration
   def change
     create_table :jobs do |t|
-      t.string  :type
+      t.string  :uid
+      t.string  :test_type
       t.integer :priority
+      t.integer :number
+      t.string  :title
+      t.string  :ssh_url
+      t.string  :branch
+      t.string  :commit
 
       t.timestamps
     end
